df617a41bf34dca5f4ff44c7603e358fb43d5b7a
berellevy <berellevy@gmail.com>
2020-10-06 09:36:16 -0400

    add snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,11 @@ import React from 'react';
 import renderer from 'react-test-renderer'
 import App from './App';
 
-// describe('App snapshot test', () => {
-//   test('App snapshot test 1', () => {
-//     const component = renderer.create(<App />);
-//     const tree = component.toJSON()
-//     expect(tree).toMatchSnapshot()
-//   })
-// });
+describe('App snapshot test', () => {
+  test('App snapshot test 1', () => {
+    const component = renderer.create(<App />);
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+});
 
