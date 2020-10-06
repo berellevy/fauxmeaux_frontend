26676bfe36e8f58964c1d91fad311b0f3ae5365b
berellevy <berellevy@gmail.com>
2020-10-06 09:33:14 -0400

    add react-test-renderer

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// describe('App snapshot test', () => {
+//   test('App snapshot test 1', () => {
+//     const component = renderer.create(<App />);
+//     const tree = component.toJSON()
+//     expect(tree).toMatchSnapshot()
+//   })
+// });
+
